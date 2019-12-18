0d95e7c9dace11baf4014188c72057428f7e6ec7
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-18 18:49:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,12 @@
 def countdown (number)
   while number < 0
   puts "#{number} SECOND(S)!"
-  number -=
+  number -= 1
 end
 
 def countdown_with_sleep (number)
   while number < 0
   puts "#{number} SECOND(S)!"
-  number -=
-  sleep 1.seconds
+  number -= 1
+  sleep(1)
 end
